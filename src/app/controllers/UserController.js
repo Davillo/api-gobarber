d44780166f98012751b1b12abcf4007de66a8f67
Davillo <davilloaurelio@hotmail.com>
2020-12-22 16:15:59 -0300

    feat: update de user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -14,8 +14,24 @@ class UserController {
   }
 
   async update(request, response){
-    console.log(request.userId);
-    return response.json();
+    const {email, oldPassword} = request.body;
+    const user = await User.findByPk(request.userId);
+
+    if(email !== user.email){
+      const userEmailAlreadyExists = await User.findOne({where: {email : request.body.email}});
+
+      if(userEmailAlreadyExists){
+        return response.status(400).json({error: 'O e-mail informado já está sendo utilizado'});
+      }
+    }
+
+    if(oldPassword && !(await user.checkPassword(oldPassword))){
+      return response.status(401).json({error: 'Senhas não conferem'})
+    }
+
+    const {id,name,provider} = await user.update(request.body);
+
+    return response.json({id,name,email,provider});
   }
 
 
