e0f0148fa962df560dfff976d9e05eb85e0478ed
Davillo <davilloaurelio@hotmail.com>
2020-12-22 09:53:20 -0300

    feat: store de user e validação se email já existe.

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,18 @@
+import User from '../models/User';
+
+class UserController {
+
+  async store(request, response){
+    const userEmailAlreadyExists = await User.findOne({where: {email : request.body.email}});
+
+    if(userEmailAlreadyExists){
+      return response.status(400).json({error: 'O e-mail informado já está sendo utilizado'});
+    }
+
+    const {id,name,email,provider} = await User.create(request.body);
+    return response.json({id,name,email,provider});
+  }
+
+}
+
+export default new UserController();
