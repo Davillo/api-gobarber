f4262f283d95d54f3a6c784d78ef9f633be45fde
Davillo <davilloaurelio@hotmail.com>
2020-12-22 00:01:08 -0300

    feat: models e database conectados

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import {Router} from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req,res) => {
-    return res.json('adasdasdasd');
+routes.get('/', async (req,res) => {
+
+    const user = await User.create({
+      name: 'diego shell',
+      email: 'diego@gmail.com',
+      password_hash: '12312312'
+    });
+    return res.json(user);
 });
 
 export default routes;
